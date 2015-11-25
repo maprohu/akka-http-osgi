07944242fc08a8be0e03bf37082ec63497a0ec40
maprohu <maprohu@gmail.com>
2015-11-25 18:09:26 +0000

    listenable actor refs? try rx

--- system/src/main/scala/akkaosgi/system/AORef.scala
+++ system/src/main/scala/akkaosgi/system/AORef.scala
@@ -1,11 +1,14 @@
 package akkaosgi.system
 
 import akka.actor.{PoisonPill, ActorRef}
+import akka.pattern.pipe
 
 /**
   * Created by pappmar on 25/11/2015.
   */
 trait AORef {
+
+  object Changed
 
   def ref = _ref
 
@@ -17,16 +20,36 @@ trait AORef {
   def register(newRef: ActorRef): Unit = {
     refs = newRef +: refs
     _ref = newRef
+
+    publish
+  }
+
+  def publish {
+    listeners foreach (_ ! Changed)
   }
 
   def unregister(oldRef: ActorRef) = {
+    val old = _ref
+
     refs = refs diff Seq(oldRef)
     _ref = refs.headOption.getOrElse(null)
+
+    if (old != _ref) publish
   }
 
   def unregisterAndStop(oldRef: ActorRef) = {
     unregister(oldRef)
     oldRef ! PoisonPill
+  }
+
+  private var listeners = Set[ActorRef]()
+
+  def listen(listener: ActorRef): Unit = {
+    listeners += listener
+  }
+
+  def unlisten(listener: ActorRef) = {
+    listeners -= listener
   }
 
 }
